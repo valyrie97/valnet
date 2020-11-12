cc1021940828d2ed8f831f8ccdba70e277e35a49
Marcus <marcus@valnet.xyz>
2020-11-11 21:47:54 -0500

    update relay

--- relay/index.js
+++ relay/index.js
@@ -5,18 +5,21 @@ const { config } = require('./../package.json');
 
 title('relay');
 
-let connection = null;
+// let connection = null;
 
-(function tryConnect() {
-	connection = net.createConnection(config.ports.ns, 'valnet.xyz', () => {
-		log.success('Connected to name server');
-	});
-	connection.on('error', () => {
-		log.error('Could not connect to name server')
-		connection = null;
-		tryConnect();
-	});
-	connection.on('data', (data) => {
-		log.debug(data.toString());
-	})
-})();
+// (function tryConnect() {
+// 	connection = net.createConnection(config.ports.ns, 'valnet.xyz', () => {
+// 		log.success('Connected to name server');
+// 	});
+// 	connection.on('error', () => {
+// 		log.error('Could not connect to name server')
+// 		connection = null;
+// 		tryConnect();
+// 	});
+// 	connection.on('data', (data) => {
+// 		log.debug(data.toString());
+// 	})
+// })();
+
+
+setTimeout(() => {}, 1000)
