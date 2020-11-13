4aa755f9ac49fbf9284e27b013182487a2eb18bc
Marcus <marcus@valnet.xyz>
2020-11-13 10:19:44 -0500

    end to end encryption

--- relay/index.js
+++ relay/index.js
@@ -1,47 +1,51 @@
+(async () => {
 const { title } = require('../lib/title');
 const net = require('net');
 const log = require('signale').scope('relay');
-const { config } = require('./../package.json');
+const { config } = require('../package.json');
 const { Identity } = require('../lib/Identity');
 const stp = require('../lib/STP');
 title('relay', false);
 
-const identity = new Identity('relay', 'default');
+const identity = await new Identity('relay', 'default');
 
 
-// let connection = null;
-
-// (function tryConnect() {
-// 	connection = net.createConnection(config.ports.ns, 'valnet.xyz', () => {
-// 		log.success('Connected to name server');
-// 	});
-// 	connection.on('error', () => {
-// 		log.error('Could not connect to name server')
-// 		connection = null;
-// 		tryConnect();
-// 	});
-// 	connection.on('data', (data) => {
-// 		log.debug(data.toString());
-// 	})
-// })();
-
-(async () => {
-	await identity.key;
-
-	const server = stp.createServer({
-		publicKey: (await identity.key).exportKey('pkcs8-public-pem'),
-		privateKey: (await identity.key).exportKey('pkcs8-private-pem')
-	}, (client) => {
-		log.info(`incomming connection from ${client.remoteAddress}`);
+// ==================================== [STP CLIENT]
+let client = null;
+(function tryConnect() {
+	client = stp.connect(identity, config.ports.relay, config.addresses.relay);
+	client.on('ready', () => {
+		log.success(`connected to ${config.addresses.relay}`);
+	})
+	client.on('error', () => {
+		log.error(`connection error on ${config.addresses.relay}`)
+		client = null;
+		setTimeout(tryConnect, 1000);
 	});
-
-	server.listen(config.ports.relay);
-	log.success(`STP server listening on ${config.ports.relay}`);
-
-	stp.connect(config.ports.relay, config.addresses.relay);
+	client.on('data', (data) => {
+		log.debug(data.toString());
+	})
 })();
+// stp.connect(config.ports.relay, config.addresses.relay);
 
 
+
+// ==================================== [STP SERVER]
+const server = stp.createServer(identity, (client) => {
+	log.info(`incomming connection from ${client.remoteAddress}
+	${client.remoteIdentity}`);
+});
+
+server.listen(config.ports.relay);
+log.success(`STP server listening on ${config.ports.relay}`);
+
+
+
+
+
+
+
+// ==================================== [EXPRESS]
 const express = require('express');
 const app = express();
 
@@ -50,3 +54,18 @@ app.get('/', (req, res) => {
 })
 
 app.listen(9999);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
